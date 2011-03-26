746b508f102e30432bfc65391bf004eef2abbb63
fzzbt <fuzzybyte@gmail.com>
2011-03-26 10:05:39 +0200

    Selkeytysta fibonacci esimerkkiin.

--- esimerkit/fib.py
+++ esimerkit/fib.py
@@ -7,9 +7,15 @@ def fib(n):
         a, b = b, a+b
     return b
 
-if __name__ == "__main__":
-    try: # Yritetään lukea kokonaisluku
-        n = int(input())
+if __name__ == "__main__": # Pääaliohjelma
+    s = input("Anna fibonacci syöttöluku: ") # Lue syöte käyttäjältä
+    
+    # input palauttaa aina stringin, mutta haluamme kokonaisluvun
+    try:
+        # Yritä muuttaa syöte kokonaislukutyypiksi
+        n = int(s) 
     except ValueError:
+    	# Tyypin muutos epäonnistui, poistu ohjelmasta virheviestillä
         sys.exit("Antamasi syöte ei ollut kokonaisluku!")
+        
     print(fib(n))
